54db1fd589a2ff2e52391fc4df35dcbb00ebaf64
LuxAndra TravelZ <ss2414336.ss@gmail.com>
2026-04-01 08:26:32 +0530

    Update Firebase configuration and initialization script

--- firebase-config.js
+++ firebase-config.js
@@ -14,3 +14,27 @@ const firebaseConfig = {
 
 firebase.initializeApp(firebaseConfig);
 const db = firebase.database();
+<script type="module">
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.11.0/firebase-app.js";
+  import { getAnalytics } from "https://www.gstatic.com/firebasejs/12.11.0/firebase-analytics.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  const firebaseConfig = {
+    apiKey: "AIzaSyAVoVjb6WcOn9sOluzfIJJueK7UJ7m2hck",
+    authDomain: "ai-power-business.firebaseapp.com",
+    databaseURL: "https://ai-power-business-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "ai-power-business",
+    storageBucket: "ai-power-business.firebasestorage.app",
+    messagingSenderId: "957300448743",
+    appId: "1:957300448743:web:988c1d2d5985ab5918b142",
+    measurementId: "G-D7W1E3XLM0"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
+</script>
