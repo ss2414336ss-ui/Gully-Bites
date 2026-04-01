cd8ab3af7d6fa6cc7f0e1bb077358f37d6f6d523
LuxAndra TravelZ <ss2414336.ss@gmail.com>
2026-04-01 07:49:49 +0530

    Add Firebase configuration to firebase-config.js
    Added Firebase configuration for project initialization.

---
+++ firebase-config.js
@@ -0,0 +1,16 @@
+// 🔥 FIREBASE CONFIG
+// Yahan apna Firebase config paste karo
+// firebase.google.com pe jaake free account banao
+
+const firebaseConfig = {
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_PROJECT.firebaseapp.com",
+  databaseURL: "https://YOUR_PROJECT-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "YOUR_PROJECT_ID",
+  storageBucket: "YOUR_PROJECT.appspot.com",
+  messagingSenderId: "YOUR_SENDER_ID",
+  appId: "YOUR_APP_ID"
+};
+
+firebase.initializeApp(firebaseConfig);
+const db = firebase.database();
